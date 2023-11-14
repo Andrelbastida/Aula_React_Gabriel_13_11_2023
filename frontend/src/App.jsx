137675855740fa8d9c764cf3223590c01afd2281
andrelbastida <andrelbastida@yahoo.com.br>
2023-11-13 22:33:53 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Cria_Tarefa from "./Components/Cria_Tarefa/Cria_Tarefa"
+import "../src/App.css"
+
+function App() {
+  return(
+    
+    <div> 
+      <h1>Hellow World</h1>
+      <Cria_Tarefa/>
+      
+    </div>
+    
+  )
+}
+
+export default App
